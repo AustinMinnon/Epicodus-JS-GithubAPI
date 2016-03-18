a558802f41645b59ae666fad3c0f27855996f2de
austinminnon <whi7ee@gmail.com>
2016-03-18 13:52:02 -0700

    add user interface logic

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,1 +1,10 @@
+var apiKey = require('./../.env').apiKey;
+var git = require('./../js/github.js');
 
+$(document).ready(function() {
+  $("#search").click( function() {
+    var username = $("#username").val();
+    git.getRepos(username);
+    event.preventDefault();
+  });
+});
